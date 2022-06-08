753ab27d5559475c635aa9073e9c1bed2b18758d
alireza faizi <86611004+alireza-fa@users.noreply.github.com>
2022-06-08 19:48:53 +0430

    Update tasks.py

--- pool/tasks.py
+++ pool/tasks.py
@@ -4,13 +4,6 @@ from bucket import bucket
 
 
 @shared_task
-def upload_image_task(user, image):
-    # Upload.objects.create(user=context['user'], image=context['image'])
-    Upload.objects.create(user=user[0], image=image[0])
-    return True
-
-
-@shared_task
 def delete_object_task(filename):
     bucket.delete_object(filename=filename)
     return True
